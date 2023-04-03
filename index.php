75ed6bbe6f06b8aa202c2fceafcced5982c78b55
Azlou77 <nguyenlouis172@gmail.com>
2023-04-03 15:55:55 +0200

    display users && display it on pdf

--- index.php
+++ index.php
@@ -10,17 +10,14 @@ $sql = "SELECT * FROM `users` ";
 $query= $db->query($sql);
 $users = $query->fetchAll();
 
-require_once 'pdf-content.php';
-die;
 //Start the buffer, no data send to the browser except the headers
 ob_start();
 require_once 'pdf-content.php';
-$html = ob_get_clean();
+$html = ob_get_contents();
 ob_end_clean();
 
+
 require_once 'vendor/autoload.php';
-
-
 
 //Make custom options
 $options = new Options();
@@ -28,9 +25,9 @@ $options = new Options();
 $options->set('defaultFont', 'Courier');
 
 //Instanciate the class
-$dompdf = new Dompdf();
+$dompdf = new Dompdf($options);
 //Load HTML content
-$dompdf->loadHtml('Hello World');
+$dompdf->loadHtml($html);
 //Changement size/position for the paper
 $dompdf->setPaper('A4', 'landscape');
 //Display the pdf
